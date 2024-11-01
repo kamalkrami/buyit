359ed9d144416e1441008f0f166848f8d785a375
nilauth <nilauth.dev@gmail.com>
2024-11-01 11:29:02 +0100

    Initializing the project

---
+++ ProductService/src/test/java/com/buyit/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyit.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
